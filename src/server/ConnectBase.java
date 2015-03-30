b7b4e71b52f25fb49aa0016624f72df2177f3117
Sergei Moskalyov <sv.moskalyov@gmail.com>
2015-03-30 22:27:38 +0300

    renew file "ConnectBase"

--- src/server/ConnectBase.java
+++ src/server/ConnectBase.java
@@ -4,43 +4,37 @@ import java.sql.*;
 
 public class ConnectBase {
 
-    public static String serverName = "localhost";
-    public static String port = "3306";
-    public static String mydatabase = "leadtime";
-    public static String username_db = "root";
-    public static String password_db = "root";
+    static Connection connection;
 
-    public static void main(String[] args) {
+    ConnectBase() {
+        setConnection();
+    }
 
+    private void setConnection() {
         try {
+            String serverName = "localhost";
+            String port = "3306";
+            String mydatabase = "leadtime";
+            String username_db = "root";
+            String password_db = "root";
+
             Class.forName("com.mysql.jdbc.Driver").newInstance();
             String url = "jdbc:mysql://" + serverName + ":" + port + "/" + mydatabase + "?characterEncoding=utf8";
 
             try {
-                Connection connection = DriverManager.getConnection(url, username_db, password_db);
-                System.out.println("Connection Susses");
-
-                Statement st = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
-
-                ResultSet rs = st.executeQuery("SELECT login, pass FROM admins WHERE id=3");
-
-                if (rs.next()) {
-
-                    System.out.println(rs.getString("login"));
-                    System.out.println(rs.getString("pass"));
-
-                }
+                connection = DriverManager.getConnection(url, username_db, password_db);
+                System.out.println("Соединение установленно!");
 
             } catch (SQLException e) {
                 e.printStackTrace();
             }
 
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    public static Connection getConnection() {
+        return connection;
     }
 }
