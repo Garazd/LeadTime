d279a3dddd28d92238ef37598cc45af62ad55eff
Garazd <GarazdVZ@gmail.com>
2015-04-05 23:18:23 +0300

    Update ConnectionDataBase.java

--- src/database/ConnectionDataBase.java
+++ src/database/ConnectionDataBase.java
@@ -2,48 +2,50 @@ package database;
 
 import java.sql.*;
 
-public class ConnectionDataBase
-{
+public class ConnectionDataBase {
 
-    public static String serverName = "localhost";  // Имя или IP-адрес сервера баз данных
-    public static String port = "3306";             // Порт сервера баз данных
-    public static String mydatabase = "test";       // Имя базы данных
-    public static String username_db = "root";      // Логин для подключения к базе данных
-    public static String password_db = "";          // Пароль для подключния к базе данных
+    static final String serverName = "localhost";
+    static final String port = "3306";
+    static final String database = "database";
+    static final String username = "root";
+    static final String password = "8021987";
+    static final String connectionUrl = "jdbc:mysql://" + serverName + ":" + port + "/" + database + "?characterEncoding=utf8";
 
-    public static void main(String[] args)
-    {
-        try
-        {
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-            String url = "jdbc:mysql://" + serverName + ":"+ port + "/" + mydatabase + "?characterEncoding=utf8";   // URL подключения
-            try
-            {
-                Connection connection = DriverManager.getConnection(url, username_db, password_db);
-                System.out.println("Connection Susses");
+    public ConnectionDataBase() {
 
-                Statement st = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+        Connection connectionDataBase = null;
+        Statement statement = null;
+        ResultSet resultSet;
 
-                ResultSet rs = st.executeQuery("SELECT id, name FROM tab_1 WHERE ID = 1");
-
-                if(rs.next()) {
-                    System.out.println(rs.getString("name"));
-
-                    rs.updateString("name", "test2");
-                    rs.updateRow();
-
-                    System.out.println(rs.getString("name"));
-                }
-
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            connectionDataBase = DriverManager.getConnection(connectionUrl, username, password);
+            System.out.println("Connect successfully");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            statement = connectionDataBase.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            resultSet = statement.executeQuery("SELECT * FROM admins");
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String login = resultSet.getString("login");
+                String password = resultSet.getString("password");
+                System.out.format("%d %s %s\n", id, login, password);
+            }
+        }
+        catch(SQLException e){
             e.printStackTrace();
         }
     }
